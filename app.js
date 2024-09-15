bfbba42e0e5b575c5298e79142e3721ff0d4dbe9
wangshangwen <shangwenwang20@gmail.com>
2024-09-15 16:00:34 +0800

    chore: readline

--- app.js
+++ app.js
@@ -1,24 +1,58 @@
-const fs = require("fs");
-const path = require("path");
+const fs = require("node:fs");
+const path = require("node:path");
+const readline = require("node:readline");
 const { NodeSSH } = require("node-ssh");
 
 const privateKeyPath = "/Users/wangshangwen/.ssh/tenyun.pem";
 const ssh = new NodeSSH();
 
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const serverLoginOpt = {
+  host: "1.15.61.169",
+  username: "root",
+  privateKeyPath: privateKeyPath,
+};
+
 async function main() {
-  const conn = await ssh.connect({
-    host: "1.15.61.169",
-    username: "root",
-    privateKeyPath: privateKeyPath,
-  });
+  const { host, username } = serverLoginOpt;
+  console.log(`Connected to the server, ${host}, ${username}`);
+  try {
+    const conn = await ssh.connect(serverLoginOpt);
+    promptForCommand(conn);
+  } catch (ex) {
+    console.error("Failed to connect:", err);
+  }
+}
 
-  const resp = await conn.exec("ls", ["-la"], {
-    cwd: "/",
-    stream: "stdout",
-    options: { pty: true },
-  });
+function promptForCommand(conn) {
+  rl.question("$ ", (command) => {
+    if (command.trim().toLowerCase() === "exit") {
+      rl.close();
+      ssh.dispose();
+      console.log("Disconnected from the server");
+      return;
+    }
 
-  console.log(resp);
+    conn
+      .execCommand(command)
+      .then((result) => {
+        const { stdout, stderr } = result;
+        if (stderr) {
+          console.error(stderr);
+        } else {
+          console.log(stdout);
+        }
+        promptForCommand(conn);
+      })
+      .catch((err) => {
+        console.error("Command execution error:", err);
+        promptForCommand(conn);
+      });
+  });
 }
 
 main().catch((ex) => console.log(ex));
